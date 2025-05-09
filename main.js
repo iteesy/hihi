759865035021e794e5f318be9b8fae630ddad7c6
Irish <iteesy714@gmail.com>
2025-05-08 17:16:11 -0700

    add pics

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const button = document.getElementById('showWare');
-let clickCount = 0;
+const images = [
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+    'images/4.png'
+];
 
-button.addEventListener('click', function() {
-  let iframe = document.getElementById('ware-iframe');
+let currentImageIndex = 0;
+const displayImage = document.getElementById('displayImage');
+const showWareButton = document.getElementById('showWare');
 
-  if (!iframe) {
-    iframe = document.createElement('iframe');
-    iframe.width = '600';
-    iframe.height = '400';
-    iframe.id = 'ware-iframe';
-    document.body.appendChild(iframe);
-  }
+function showNextImage() {
+    // hide current pic
+    displayImage.style.display = 'none';
+    
+    // update to next image
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    
+    // set new pic
+    displayImage.src = images[currentImageIndex];
+    displayImage.style.display = 'block';
+}
 
-  clickCount++;
-
-  if (clickCount === 1) {
-    iframe.src = 'https://www.ssense.com/en-us/women/product/ashley-williams/ssense-exclusive-white-bow-bonnet/16060511';
-  } else if (clickCount === 2) {
-    iframe.src = 'https://www.ssense.com/en-us/women/product/lemaire/khaki-wadded-vest/13949361';
-  } else if (clickCount === 3) {
-    iframe.src = 'https://www.ssense.com/en-us/men/product/jw-anderson/orange-long-sleeve-t-shirt/17711331';
-  }
-});
+showWareButton.addEventListener('click', showNextImage);
